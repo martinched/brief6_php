0789c75d401d8cbbbb4438c41ddba10c962bea84
martinched <147699516+martinched@users.noreply.github.com>
2024-01-24 16:24:54 +0100

    requetSQL cat dom

--- src/index.php
+++ src/index.php
@@ -1,11 +1,80 @@
+
 <?php include('header.php'); 
 include("pdo.php");
-$result = $pdo->query("SELECT * FROM favori");
-    $favori = $result->fetchAll(PDO::FETCH_ASSOC); 
+
+$result = $pdo->query("SELECT * FROM favori");  //Déclare la veriable $result et lui assigne la valeur de la requete SQL 
+$favori = $result->fetchAll(PDO::FETCH_ASSOC);  //Déclare la veriable $favori et lui assigne la valeur de $result
     
-?>  
+$result = $pdo->query("SELECT * FROM categorie");   //Assigne a $result la valeur de la requete SQL  
+$categories = $result->fetchAll(PDO::FETCH_ASSOC); //Déclare la veriable $categories et lui assigne la valeur de $result
+
+$result = $pdo->query("SELECT * FROM domaine");   //Assigne a $result la valeur de la requete SQL $result 
+$domaines = $result->fetchAll(PDO::FETCH_ASSOC);   //Déclare la variable $categories et lui assigne la valeur de $result
+
+$requeteSQL = "";   //Déclare la veriable $requeteSQL et lui assigne une valeur nulle 
+
+
+//Déclare une boucle 'if' 
+
+if (!empty($_GET['filtreCategorie'])){  // le parametre exprime: "si la valeur de la globale $_GET n'est pas vide alors..."
+
+    $requeteSQL = "SELECT * FROM favori    /*  ... assigne, a $requeteSQL, le contenu de la table 'favori'... */ 
+    INNER JOIN `cat-fav`                    /* ...lié a la table d'association cat-fav..  */
+    ON favori.id_fav = `cat-fav`.`id_fav`   /**...par l'id_fav de la table favori et l'id_fav de la table cat-fav... */
+
+    INNER JOIN `categorie`                   /*...lié a la table 'categorie'... */ 
+    ON categorie.id_cat = `cat-fav`.id_cat    /** ...par l'id_cat de la table categorie et l'id_cat de la table cat-fav... */ 
+
+    INNER JOIN domaine                          /*...lié a la table 'domaine '... */ 
+    ON favori.id_dom = domaine.id_dom           /** ...par l'id_dom de la table favori et l'id_cat de la table domaine ...*/ 
+
+    WHERE categorie.id_cat = " . $_GET['filtreCategorie']  //...du moment que l'id_cat de la table categorie est égal a la valeur de $_GET
+    . " AND domaine.id_dom = " . $_GET['filtreDomaine'];
+
+} else {                                                        //ou alors affiche la totalité de la table favori
+  $requeteSQL = "SELECT * FROM favori" ; 
+};
+
+$result = $pdo->query($requeteSQL);                     //Assigne a $result la valeur de $requeteSQL  
+$favori = $result->fetchAll(PDO::FETCH_ASSOC);          //Assigne a $favori la valeur de $result 
+
+?> 
+
+
 <section id="bookmarks">
-        <div class="flex justify-end"><button class="bouton ">Ajouter un favori</button></div>
+        <div class="justify-between flex-row">
+            <form method="GET" action="">
+                <div>
+                    <label for="filtreCategorie">Choisissez une catégorie:</label>
+                    <select name="filtreCategorie" id="filtreCategorie">
+                        <option valeur="">--toutes--</option>
+                        <?php
+                            foreach ($categories as $categorie) { ?>
+                            <option value="<?php echo $categorie['id_cat']?>">
+                                <?php echo $categorie['nom']?>
+                            </option>
+                        <?php } ?>
+                    </select> 
+                </div>
+                <div>
+                <label for="filtreDomaine">Choisissez un domaine:</label>
+                    <select name="filtreDomaine" id="filtreDomaine">
+                        <option valeur="">--toutes--</option>
+                        <?php
+                            foreach ($domaines as $domaine) { ?>
+                            <option value="<?php echo $domaine['id_dom']?>">
+                                <?php echo $domaine['nom']?>
+                            </option>
+                        <?php } ?>
+                    </select> 
+                </div>
+
+                <input type="submit" value="Filtrer">    
+
+            </form>
+            <button class="bouton ">Ajouter un favori</button>
+        </div>
+
         <table class="table-fav">
             <tr class="border-separate border border-slate-500 border-indigo-500/100 ">
                 <th class="border-separate border border-slate-500 border-indigo-500/100 px-3 text-center">id_fav</th>
